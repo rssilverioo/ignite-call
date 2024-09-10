8ae7448ecc84755f8c52a63f8b3ba2cd45d50a76
Rodrigo Silverio <rodrigo.s.silverio@outlook.comgit config --global user.email rodrigo.s.silverio@outlook.com>
2024-09-09 21:03:44 -0700

    settings cookies and start Connect-calendar

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { prisma } from '@/src/lib/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,7 +10,6 @@ export default async function handler(
   if (req.method !== 'POST') {
     return res.status(405).end()
   }
-
   const { name, username } = req.body
 
   const userExists = await prisma.user.findUnique({
@@ -27,6 +27,11 @@ export default async function handler(
       name,
       username,
     },
+  })
+
+  setCookie({ res }, '@ignitecall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
   })
   return res.status(201).json(user)
 }
